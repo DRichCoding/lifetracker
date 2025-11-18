1c4778c96db86f522574135bc632a98e83d851b9
Dan <dt_Dan0707@icloud.com>
2025-11-18 07:06:55 +0000

    5 commit

--- public/QuoteBook.js
+++ public/QuoteBook.js
@@ -1,21 +1,7 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc } from "firebase/firestore";
+// quoteBook.js
+import { db } from "./firebase.js";
+import { collection, addDoc, getDocs, deleteDoc, doc } from "firebase/firestore";
 
-// Your Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyCdxLfIfQL-M18y8zQl5aE1z3AccNWslBk",
-  authDomain: "lifetrackerid.firebaseapp.com",
-  projectId: "lifetrackerid",
-  storageBucket: "lifetrackerid.firebasestorage.app",
-  messagingSenderId: "76945546748",
-  appId: "1:76945546748:web:f5bc9009e19640226f2679"
-};
-
-// Initialize Firebase + Firestore
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// References
 const quoteInput = document.getElementById("quoteInput");
 const saveBtn = document.getElementById("saveBtn");
 const showBtn = document.getElementById("showBtn");
@@ -33,13 +19,12 @@ saveBtn.addEventListener("click", async () => {
 
 // Show all quotes
 showBtn.addEventListener("click", async () => {
-  quoteList.innerHTML = ""; // clear list
+  quoteList.innerHTML = "";
   const snapshot = await getDocs(collection(db, "quotes"));
   snapshot.forEach((docSnap) => {
     const li = document.createElement("li");
     li.textContent = docSnap.data().text;
 
-    // Delete button
     const delBtn = document.createElement("button");
     delBtn.textContent = "Delete";
     delBtn.onclick = async () => {
--- public/index.js
+++ public/index.js
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCdxLfIfQL-M18y8zQl5aE1z3AccNWslBk",
   authDomain: "lifetrackerid.firebaseapp.com",
@@ -14,7 +11,5 @@ const firebaseConfig = {
   appId: "1:76945546748:web:f5bc9009e19640226f2679"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
