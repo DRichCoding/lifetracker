c108c31d2d816740b59033ef76e0ab8ae0993d34
Dan <dt_Dan0707@icloud.com>
2025-11-18 07:12:32 +0000

    6 commit

--- public/index.js
+++ public/index.js
@@ -4,9 +4,9 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCdxLfIfQL-M18y8zQl5aE1z3AccNWslBk",
-  authDomain: "lifetrackerid.firebaseapp.com",
-  projectId: "lifetrackerid",
-  storageBucket: "lifetrackerid.firebasestorage.app",
+  authDomain: "life-tracker-dan.firebaseapp.com",
+  projectId: "life-tracker-dan",
+  storageBucket: "life-tracker-dan.firebasestorage.app",
   messagingSenderId: "76945546748",
   appId: "1:76945546748:web:f5bc9009e19640226f2679"
 };
